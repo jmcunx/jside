9867925de1c77474f86bbeb52da557280ef89db7
John McCue <jmccue@jmcunx.com>
2022-05-07 19:38:56 -0400

    Fixed some minor memory issues. Added ability to test with valgrind

--- jside.c
+++ jside.c
@@ -76,6 +76,9 @@ int main(int argc, char **argv)
   
   close_out(&(w.out));
   close_out(&(w.err));
+
+  if (w.prog_name != (char *) NULL)
+    free(w.prog_name);
   exit(EXIT_SUCCESS);
 
 }  /* main() */
